2abb40aa6826593c339e7417e783998b85c96fd3
Justin4587 <Thurman@atlasok.com>
2021-05-14 12:21:33 -0500

    1.js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,5 +1,16 @@
 // Input checkbox tuning
-let adict = {};
-let alist = [];
+const adict = {};
+const alist = [];
+$('.amenities-list').change(function (event) {
+  const IDofamen = this.getattribute('data-id');
+  const Nameofamen = this.getattribute('data-name');
 
-
+  if (event.target.checked) {
+    adict[IDofamen] = Nameofamen;
+    alist.push(Nameofamen);
+  } else {
+    delete adict[IDofamen];
+    alist.splice(alist.indexOf(Nameofamen), 1);
+  }
+  $('.amenities h4').text(alist);
+});

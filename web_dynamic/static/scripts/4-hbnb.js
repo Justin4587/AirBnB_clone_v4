cd2148243a6f2800acb23059cf8a1a3256c1bf9e
Justin4587 <Thurman@atlasok.com>
2021-05-15 13:46:57 -0500

    Task 5 api status = ok

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -18,25 +18,25 @@ $(function () {
 });
 
 // api_status = "OK" check
-  $.get('http://0.0.0.0:5001/api/v1/status/', function (info, status) {
-    if (status === 'OK') {
-      $('#api_status').addClass('available');
-    } else {
-      $('#api_status').removeClass('available');
-    }
-  });
+$.get('http://0.0.0.0:5001/api/v1/status/', function (info, status) {
+  if (status === 'OK') {
+    $('#api_status').addClass('available');
+  } else {
+    $('#api_status').removeClass('available');
+  }
+});
 
 // task 4: 3.js
-  $.ajax({
-    type: 'POST',
-    url: 'http://0.0.0.0:5001/api/v1/places_search/',
-    contentType: 'application/json',
-    data: '{}',
-    success: function (info) {
-      for (let i = 0; i < info.length; i++) {
-        const place = info[i];
-        $('.places').append(
-          '<article><div class="title_box"><h2>' + place.name +
+$.ajax({
+  type: 'POST',
+  url: 'http://0.0.0.0:5001/api/v1/places_search/',
+  contentType: 'application/json',
+  data: '{}',
+  success: function (info) {
+    for (let i = 0; i < info.length; i++) {
+      const place = info[i];
+      $('.places').append(
+        '<article><div class="title_box"><h2>' + place.name +
               '</h2><div class="price_by_night">$' + place.price_by_night +
               '</div></div><div class="information"><div class="max_guest">' +
               rmAddS(place.max_guest, 'Guest') +
@@ -46,27 +46,27 @@ $(function () {
               rmAddS(place.number_bathrooms, 'Bathroom') +
               '</div></div><div class="description">' +
               place.description + '</div></article>');
-      }
     }
-  });
-
-  function rmAddS (num, name) {
-    if (num === 1) { return `${num} ${name}`; } else { return `${num} ${name}s`; }
   }
+});
+
+function rmAddS (num, name) {
+  if (num === 1) { return `${num} ${name}`; } else { return `${num} ${name}s`; }
+}
 
 // task 5: 4.js
-  $('[type=button]').click(function () {
-    $('.places').empty();
-    $.ajax({
-      type: 'POST',
-      url: 'http://0.0.0.0:5001/api/v1/places_search/',
-      contentType: 'application/json',
-      data: JSON.stringify({ amenities: (adict.map(item => item.IDofamen)) }),
-      success: function (info) {
-        for (let i = 0; i < info.length; i++) {
-          const place = info[i];
-          $('.places').append(
-            '<article><div class="title_box"><h2>' + place.name +
+$('[type=button]').click(function () {
+  $('.places').empty();
+  $.ajax({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    contentType: 'application/json',
+    data: JSON.stringify({ amenities: (adict.map(item => item.IDofamen)) }),
+    success: function (info) {
+      for (let i = 0; i < info.length; i++) {
+        const place = info[i];
+        $('.places').append(
+          '<article><div class="title_box"><h2>' + place.name +
                 '</h2><div class="price_by_night">$' + place.price_by_night +
                 '</div></div><div class="information"><div class="max_guest">' +
                 rmAddS(place.max_guest, 'Guest') +
@@ -76,7 +76,7 @@ $(function () {
                 rmAddS(place.number_bathrooms, 'Bathroom') +
                 '</div></div><div class="description">' +
                 place.description + '</div></article>');
-        }
       }
-    });
+    }
   });
+});

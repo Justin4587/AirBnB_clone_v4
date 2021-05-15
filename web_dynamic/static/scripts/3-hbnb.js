423b0aa4a2ce1e1205bc96a22b5dae8aae95b447
kwhit2 <kfw2@outlook.com>
2021-05-15 19:24:56 +0000

    API, button & search are now working for task 4 and 5 fully

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -15,28 +15,27 @@ $(function () {
     }
     $('.amenities h4').html(alist.join(', '));
   });
-});
 
-// api_status = "OK" check
-$.get('http://0.0.0.0:5001/api/v1/status/', function (info, status) {
-  if (status === 'OK') {
-    $('#api_status').addClass('available');
-  } else {
-    $('#api_status').removeClass('available');
-  }
-});
+  // api_status = "OK" check
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (info) {
+    if (info.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
 
-// task 4: 3.js
-$.ajax({
-  type: 'POST',
-  url: 'http://0.0.0.0:5001/api/v1/places_search/',
-  contentType: 'application/json',
-  data: '{}',
-  success: function (info) {
-    for (let i = 0; i < info.length; i++) {
-      const place = info[i];
-      $('.places').append(
-        '<article><div class="title_box"><h2>' + place.name +
+  // task 4: 3.js
+  $.ajax({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    contentType: 'application/json',
+    data: '{}',
+    success: function (info) {
+      for (let i = 0; i < info.length; i++) {
+        const place = info[i];
+        $('.places').append(
+          '<article><div class="title_box"><h2>' + place.name +
             '</h2><div class="price_by_night">$' + place.price_by_night +
             '</div></div><div class="information"><div class="max_guest">' +
             rmAddS(place.max_guest, 'Guest') +
@@ -46,10 +45,11 @@ $.ajax({
             rmAddS(place.number_bathrooms, 'Bathroom') +
             '</div></div><div class="description">' +
             place.description + '</div></article>');
+      }
     }
+  });
+
+  function rmAddS (num, name) {
+    if (num === 1) { return `${num} ${name}`; } else { return `${num} ${name}s`; }
   }
 });
-
-function rmAddS (num, name) {
-  if (num === 1) { return `${num} ${name}`; } else { return `${num} ${name}s`; }
-}
